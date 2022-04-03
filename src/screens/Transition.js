12f004ef1425ac312467ffd6f80213712ed2ba73
Ömer Esmer <68515357+Skipperlla@users.noreply.github.com>
2022-04-03 19:35:28 +0300

    Update Transition.js

--- src/screens/Transition.js
+++ src/screens/Transition.js
@@ -1,8 +1,5 @@
-import React, { useTransition, useState } from 'react'
-import CardLists from '../CardLists';
+import React, { useTransition, useState,useId } from 'react'
 import { filterProducts, generatorList } from '../data';
-
-
 
 const Transition = () => {
   const [isPending, startTransition] = useTransition({ timeoutMs: 5000 });
@@ -20,10 +17,28 @@ const Transition = () => {
     <div className="mx-auto flex items-center flex-col mt-4" style={{ width: "30rem" }}>
       <input value={filterTerm} onChange={updateFilterHandler} className="bg-white w-full" />
       {isPending ? <div className="text-white">Loading...</div> :
-        <CardLists list={filteredProducts} />
+        <CardList list={filteredProducts} />
       }
     </div>
   )
 }
+
+const CardList = ({ list }) => {
+  return (
+    <ul className="m-8 w-full list-none p-0">
+      {list?.map((item) => {
+        return (
+          <li
+            key={useId()}
+            className="my-4 rounded-xl bg-red-500 p-4 text-center text-white"
+          >
+            {item}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
 
 export default Transition
